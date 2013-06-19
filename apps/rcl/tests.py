4eb9cc1fd2d0955215c118e04da21bb1bca61a4c
sromero1984 <santi.romero@gmail.com>
2013-06-19 17:34:41 -0300

    Added .gitignore and ignored settings_local.py Updated rcl tests

--- apps/rcl/tests.py
+++ apps/rcl/tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 # models
 from rcl.models import Community
+# exceptions
+from django.db import IntegrityError
 
 
 class ClassTest(TestCase):
+    def setUp(self):
+        pass
+
     def test_community_creation_minimum_info(self):
         """
-        Just creates a Community with minimum information and
+        Just creates a Community with some information and
         saves the instance.
         """
         c = Community(
@@ -16,12 +21,14 @@ class ClassTest(TestCase):
         )
         c.save()
 
-    def test_community_name_required(self):
+    def test_community_url_name_unique(self):
         """
         Tries to save a Community without name, checks it's not possible
         """
-        c = Community(
-            kind=Community.LAND,
-            url_name='test-url'
-        )
+        c = Community(url_name='test-url')
         c.save()
+        c = Community(url_name='test-url')
+        try:
+            c.save()
+        except IntegrityError:
+            pass
